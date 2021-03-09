a570dca2a799acc058865d0ac947a98b72b889c0
GustavoTeofilo <leonardo@software-ltda.com.br>
2021-03-09 06:11:59 -0300

    Trabalhando com JS Básico

---
+++ assets/css/listaDeTarefas.js
@@ -0,0 +1,21 @@
+const criarTarefa = (evento) => {
+
+    evento.preventDefault()
+    const lista = document.querySelector('[data-list]')
+    const input = document.querySelector('[data-form-input]');
+    const valor = input.value;
+
+    const conteudo = `<p class="content">${valor}</p>`
+
+    const tarefa = document.createElement("li");
+    tarefa.classList.add('task');
+
+    tarefa.innerHTML = conteudo;
+    lista.appendChild(tarefa);
+
+    input.value = " ";
+}
+
+const novaTarefa = document.querySelector('[data-form-button]')
+
+novaTarefa.addEventListener('click', criarTarefa)
