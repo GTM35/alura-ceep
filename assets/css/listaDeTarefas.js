893b69a61f095705004cd1c48680bd818dccb65e
GustavoTeofilo <leonardo@software-ltda.com.br>
2021-03-09 06:43:11 -0300

    Adicionando botão de concluir tarefa

--- assets/css/listaDeTarefas.js
+++ assets/css/listaDeTarefas.js
@@ -1,21 +1,49 @@
-const criarTarefa = (evento) => {
+(() => {
+    
+    const criarTarefa = (evento) => {
 
     evento.preventDefault()
-    const lista = document.querySelector('[data-list]')
-    const input = document.querySelector('[data-form-input]');
-    const valor = input.value;
 
+    const lista = document.querySelector('[data-list]')
+    const input = document.querySelector('[data-form-input]')
+    const valor = input.value
+
+    const tarefa = document.createElement('li')
+    tarefa.classList.add('task')
     const conteudo = `<p class="content">${valor}</p>`
 
-    const tarefa = document.createElement("li");
-    tarefa.classList.add('task');
+    tarefa.innerHTML = conteudo
 
-    tarefa.innerHTML = conteudo;
-    lista.appendChild(tarefa);
+    tarefa.appendChild(BotaoConclui())
+    lista.appendChild(tarefa)
+    input.value = " "
 
-    input.value = " ";
 }
+
 
 const novaTarefa = document.querySelector('[data-form-button]')
 
 novaTarefa.addEventListener('click', criarTarefa)
+
+const BotaoConclui = () => { 
+    const botaoConclui = document.createElement('button')  
+    
+    botaoConclui.classList.add('check-button')
+    botaoConclui.innerText = 'concluir'
+
+    botaoConclui.addEventListener('click', concluirTarefa)
+
+    return botaoConclui
+
+}
+
+
+
+const concluirTarefa = (evento) => {
+    const botaoConclui = evento.target
+
+    const tarefaCompleta = botaoConclui.parentElement
+
+    tarefaCompleta.classList.toggle('done')
+}
+})()
